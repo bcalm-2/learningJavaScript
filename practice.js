1b38a0f1a3161ffc9e4ffbbcafdc0bbf4e186287
DwivediSrashti6@gmail.com <DwivediSrashti6@gmail.com>
2024-09-22 14:18:42 +0530

    Added operators.js

---
+++ practice.js
@@ -0,0 +1,13 @@
+const objectValue = {
+    "firstName": "Vikram",
+    "secondName": "Singh",
+    "id":"20",
+    "address":"mera ghar nhi bta rhe jao",
+}
+console.log("my name is",objectValue.firstName,
+    objectValue.secondName,". my id is", objectValue.id,". my address is",
+     objectValue.address)
+
+
+     objectValue["hobby"] = "cricket";
+     console.log("my hobby is",objectValue.hobby)
