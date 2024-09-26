eb79ba7e2313268d9c3163b9f51569bde1ed86d3
DwivediSrashti6@gmail.com <DwivediSrashti6@gmail.com>
2024-09-26 20:28:29 +0530

    add some new project

---
+++ average.js
@@ -0,0 +1,16 @@
+let name1 = Number(prompt("Enter name"));
+console.log(Number);
+const num1 = Number.parseInt(prompt("Enter number"));
+if (num1 <= 100 && num1 > 90) {
+  console.log("A");
+} else if (num1 <= 90 && num1 > 80) {
+  console.log("B");
+} else if (num1 <= 80 && num1 > 70) {
+  console.log("C");
+} else if (num1 <= 70 && num1 > 60) {
+  console.log("D");
+} else if (num1 <= 60 && num1 > 60) {
+  console.log("E");
+} else {
+  console.log("Fail");
+}
---
+++ star.js
@@ -0,0 +1,5 @@
+const num1 = Number.parseInt(prompt("Enter number"));
+for (let index = 1; index < num1; index++) {
+  let str = "*";
+  console.log(str.repeat(index));
+}
